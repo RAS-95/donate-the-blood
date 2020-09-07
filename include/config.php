90c7fdd6c7845bd3745a7e1614d1bfc9949efd82
RAS-95 <rafiahammedsongram@gmail.com>
2020-09-08 01:30:56 +0600

    Updated config

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$connection = mysqli_connect("sql2.freesqldatabase.com","sql2364304","","sql2364304") or die("Database is not connected".mysqli_connect_error());
+$connection = mysqli_connect("sql2.freesqldatabase.com:3306","sql2364304","","sql2364304") or die("Database is not connected".mysqli_connect_error());
 
 
 
