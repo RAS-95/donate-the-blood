cd6cad0ce3cec977363cad9aa51c5267edf9f4c4
RAS-95 <rafiahammedsongram@gmail.com>
2020-09-08 03:26:53 +0600

    Updated dob manipulation

--- user/update.php
+++ user/update.php
@@ -25,7 +25,7 @@
 			$city = $row['city'];
 			$dob = $row['dob'];
 
-			$date = explode("-",$dob);
+			$date = explode("-",$dob); // yyyy-mm-dd
 			$dbpassword = $row['password'];
 		}
 	}
@@ -402,7 +402,7 @@
               </select>
               <select class="form-control demo-default" name="month" id="month" style="margin-bottom:10px;" required>
 				<option value="">---Month---</option>
-				<?php if(isset($month['1'])) echo '<option option selected = "" value ="'.$month[1].'"> '.$month['1'].'</option>';?>
+				<?php if(isset($month['1'])) echo '<option option selected = "" value ="'.$date[1].'"> '.$date['1'].'</option>';?>
 			
 				
                 <option value="01" >January</option><option value="02" >February</option><option value="03" >March</option><option value="04" >April</option><option value="05" >May</option><option value="06" >June</option><option value="07" >July</option><option value="08" >August</option><option value="09" >September</option><option value="10" >October</option><option value="11" >November</option><option value="12" >December</option>
@@ -410,7 +410,7 @@
               <select class="form-control demo-default" id="year" name="year" style="margin-bottom:10px;" required>
 				<option value="">---Year---</option>
 
-				<?php if(isset($year['0'])) echo '<option option selected = "" value ="'.$year[0].'"> '.$year['0'].'</option>';?>
+				<?php if(isset($year['0'])) echo '<option option selected = "" value ="'.$date[0].'"> '.$date['0'].'</option>';?>
 			
                 <option value="1957" >1957</option><option value="1958" >1958</option><option value="1959" >1959</option><option value="1960" >1960</option><option value="1961" >1961</option><option value="1962" >1962</option><option value="1963" >1963</option><option value="1964" >1964</option><option value="1965" >1965</option><option value="1966" >1966</option><option value="1967" >1967</option><option value="1968" >1968</option><option value="1969" >1969</option><option value="1970" >1970</option><option value="1971" >1971</option><option value="1972" >1972</option><option value="1973" >1973</option><option value="1974" >1974</option><option value="1975" >1975</option><option value="1976" >1976</option><option value="1977" >1977</option><option value="1978" >1978</option><option value="1979" >1979</option><option value="1980" >1980</option><option value="1981" >1981</option><option value="1982" >1982</option><option value="1983" >1983</option><option value="1984" >1984</option><option value="1985" >1985</option><option value="1986" >1986</option><option value="1987" >1987</option><option value="1988" >1988</option><option value="1989" >1989</option><option value="1990" >1990</option><option value="1991" >1991</option><option value="1992" >1992</option><option value="1993" >1993</option><option value="1994" >1994</option><option value="1995" >1995</option><option value="1996" >1996</option><option value="1997" >1997</option><option value="1998" >1998</option><option value="1999" >1999</option><option value="2000" >2000</option><option value="2001" >2001</option><option value="2002" >2002</option>
               </select>
