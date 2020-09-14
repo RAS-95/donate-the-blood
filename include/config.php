5cb09d438298c56960150f90005dc1b82ebece59
ParthoKR <47151780+ParthoKR@users.noreply.github.com>
2020-09-14 16:19:32 +0600

    Updated with new db

--- include/config.php
+++ include/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect("sql2.freesqldatabase.com:3306","sql12365527","91b67gnFRP","sql12365527") or die("Database is not connected".mysqli_connect_error());
+$connection = mysqli_connect("sql12.freesqldatabase.com:3306","sql12365527","91b67gnFRP","sql12365527") or die("Database is not connected".mysqli_connect_error());
 
 
 ?>
