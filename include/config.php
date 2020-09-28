d867f857940591ccfc386d755d1b52c1fcb88ecf
Rafi Ahammed Songram <rafiahammedsongram@gmail.com>
2020-09-28 21:19:08 +0600

    Updated config

--- include/config.php
+++ include/config.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 $connection = mysqli_connect("localhost","id14987029_rasdtb","_iBrM*?v7q_[6$C","id14987029_donatetheblood") or die("Database is not connected".mysqli_connect_error());
-=======
-$connection = mysqli_connect("sql12.freesqldatabase.com:3306","sql12365527","91b67gnFRP","sql12365527") or die("Database is not connected".mysqli_connect_error());
->>>>>>> 5cb09d438298c56960150f90005dc1b82ebece59
 
 
 ?>
